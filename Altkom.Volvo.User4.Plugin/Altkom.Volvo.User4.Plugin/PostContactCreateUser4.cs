a3d48c9b94d57dcab2a8cec19d1f1cf2e2a5b3e2
a261620 <a261620@PLWROW912338>
2017-11-22 15:54:25 +0100

    plugins

--- Altkom.Volvo.User4.Plugin/Altkom.Volvo.User4.Plugin/PostContactCreateUser4.cs
+++ Altkom.Volvo.User4.Plugin/Altkom.Volvo.User4.Plugin/PostContactCreateUser4.cs
@@ -14,17 +14,45 @@ namespace Altkom.Volvo.User4.Plugin
             var tracingService = serviceProvider.GetService(typeof(ITracingService)) as ITracingService;
             var pluginExecutionContext = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
             var organizationServiceFactory = serviceProvider.GetService(typeof(IOrganizationServiceFactory)) as IOrganizationServiceFactory;
+            try
+            {
+                tracingService.Trace("Starting Post Contact Create");//nadawanie logow podczas wykonywania pluginu
 
-            tracingService.Trace("Starting Post Contact Create");//nadawanie logow podczas wykonywania pluginu
+                var administratorService = organizationServiceFactory.CreateOrganizationService(null);//null-administrator, guid-uzytkownik powolanie kontekstu
 
-            var administratorService = organizationServiceFactory.CreateOrganizationService(null);//null-administrator, guid-uzytkownik powolanie kontekstu
+                var target = (pluginExecutionContext.InputParameters["Target"] as Entity).ToEntity<alt_user4contact>();//kazda encja dziedziczy z entity //depth-jak gleboko zagniezdzamy sie podczas wykonywania pluginu (plugin1-depth 1, plugin 2 -depth 2); 
 
-            var target= pluginExecutionContext.InputParameters["Target"] as Entity;//kazda encja dziedziczy z entity //depth-jak gleboko zagniezdzamy sie podczas wykonywania pluginu (plugin1-depth 1, plugin 2 -depth 2); 
-            var targetName = target.Attributes["alt_name"] as string; //late bound, Formatted values-np mozemy pobrac date w formacie uzytkownika, jezyku, itp,     //input parameters-parametry wejsciowe, isolation mode-sandbox lub nie; mode-async/sync;shared variables-info miedzy pluginami
+                // var targetName = target.Attributes["alt_name"] as string; //late bound, Formatted values-np mozemy pobrac date w formacie uzytkownika, jezyku, itp,     //input parameters-parametry wejsciowe, isolation mode-sandbox lub nie; mode-async/sync;shared variables-info miedzy pluginami
+                if (target.alt_name.Contains("Error"))
+                {
+                    throw new InvalidPluginExecutionException("There was an error in entity name");
+                }
 
-            tracingService.Trace("Gathered name {0}.", targetName);
-           
+               // var postImage(pluginExecutionContext.Inp)
+                var service = organizationServiceFactory.CreateOrganizationService(pluginExecutionContext.UserId);
+                var contactId = service.Create(new Contact()
+                {
+                    FirstName = "Jan",
+                    LastName = target.alt_name
+                });
 
+                tracingService.Trace("Created contact with Id{0}", contactId);
+
+                service.Update(new alt_user4contact()
+                {
+                    Id = target.Id,
+                    alt_referencedcontactid = new EntityReference(Contact.EntityLogicalName, contactId)
+                });
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
---
+++ Altkom.Volvo.User4.Plugin/Altkom.Volvo.User4.Plugin/PreContactCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom.Volvo.User4.Plugin
+{
+    class PreContactCreate
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var tracingService = serviceProvider.GetService(typeof(ITracingService)) as ITracingService;
+            var pluginExecutionContext = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+            var organizationServiceFactory = serviceProvider.GetService(typeof(IOrganizationServiceFactory)) as IOrganizationServiceFactory;
+            var target = (pluginExecutionContext.InputParameters["Target"] as Entity).ToEntity<alt_user4contact>();
+
+            target.alt_name = target.alt_name + "added in plugin";
+        }
+    }
+}
