2281ad1efb398fe19d11db62210a894da2867140
a261620 <a261620@PLWROW912338>
2017-11-22 13:56:58 +0100

    init

---
+++ Altkom.Volvo.User4.Plugin/Altkom.Volvo.User4.Plugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom.Volvo.User4.Plugin
+{
+    public class Class1
+    {
+    }
+}
---
+++ Altkom.Volvo2.Plugin/Altkom.Volvo2.Plugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom.Volvo2.Plugin
+{
+    public class Class1
+    {
+    }
+}
